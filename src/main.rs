fc963418e7aa79f1f72da9ca9e4c7c657685fd91
Ellis Squires <esquires1@sheffield.ac.uk>
2020-03-05 22:46:59 +0000

    Add a simple no_std test harness

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,24 @@
 #![no_std]
 // Ovewriting the main application entry point
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+/* the test harness works by redefining the main function
+but there isnt one so we need to point it to a new a function name
+which can be called in _start()
+*/
+#![reexport_test_harness_main = "test_main"]
 
+// Modules
 mod vga_buffer;
-
 
 // Disable name mangling so the compiler doesnt generate a unique name
 #[no_mangle]
 // This is the overidden entry point
 pub extern "C" fn _start() -> ! {
-
-    println!("Hello");
-    println!("Println now works!");
-
-    panic!("Some panic message");
-
+    // Call the test harness
+    #[cfg(test)]
+    test_main();
     loop {}
 
 }
@@ -34,4 +38,12 @@ fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
